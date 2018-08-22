d22b86b877aa9de509d5784c6b9f462a5c6f39bb
s2kinfra <daniel.skevarp@me.com>
2018-08-22 13:02:04 +0200

    Update Flash.swift

--- Sources/Flash/Models/Flash.swift
+++ Sources/Flash/Models/Flash.swift
@@ -6,18 +6,37 @@ public final class Flash: Codable {
         case success
         case info
         case warning
+        
+        var bootstrapClass: BootstrapClass {
+            switch self {
+                case .error: return .danger
+                case .success: return .success
+                case .info: return .info
+                case .warning: return .warning
+            }
+        }
+    }
+    
+    enum BootstrapClass: String {
+        case danger
+        case warning
+        case success
+        case info
     }
 
     public var kind: Kind
     public var message: String
+    public var bootstrapClass: String
 
     public init(kind: Kind, message: String) {
         self.kind = kind
         self.message = message
+        self.bootstrapClass = kind.bootstrapClass.rawValue
     }
 
     public init(_ kind: Kind, _ message: String) {
         self.kind = kind
         self.message = message
+        self.bootstrapClass = kind.bootstrapClass.rawValue
     }
 }
