b1348c2fef02d65501506fa500b9cc2d22a8ae78
s2kinfra <daniel.skevarp@me.com>
2018-08-22 13:01:11 +0200

    Update Response+Flash.swift

--- Sources/Flash/Extensions/Response+Flash.swift
+++ Sources/Flash/Extensions/Response+Flash.swift
@@ -1,10 +1,14 @@
 import Vapor
 
 public extension Response {
-    public func flash(_ type: Flash.Kind, _ message: String) -> Response {
-        if let container = try? privateContainer.make(FlashContainer.self) {
-            container.flashes.append(.init(type, message))
+    public func flash(_ type: Flash.Kind, _ message: String, _ session: Session) -> Response {
+        let flash = Flash(type, message)
+        let jsonEncoder = JSONEncoder()
+        if let flashData = try? jsonEncoder.encode(flash) {
+            let stringData = String(data: flashData, encoding: .utf8)
+            session["_flash"] = stringData
         }
+
         return self
     }
 }
