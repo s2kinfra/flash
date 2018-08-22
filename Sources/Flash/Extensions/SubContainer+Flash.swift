13baa59d5ce3c8db85944eb61fe8d16d3a640733
s2kinfra <daniel.skevarp@me.com>
2018-08-22 13:01:29 +0200

    Update SubContainer+Flash.swift

--- Sources/Flash/Extensions/SubContainer+Flash.swift
+++ Sources/Flash/Extensions/SubContainer+Flash.swift
@@ -1,10 +1,15 @@
+
 import Vapor
 
 extension SubContainer {
-    public func flash(_ type: Flash.Kind, _ message: String) -> SubContainer {
-        if let container = try? self.make(FlashContainer.self) {
-            container.flashes.append(.init(type, message))
+    public func flash(_ type: Flash.Kind, _ message: String, _ session: Session) -> SubContainer {
+        let flash = Flash(type, message)
+        let jsonEncoder = JSONEncoder()
+        if let flashData = try? jsonEncoder.encode(flash) {
+            let stringData = String(data: flashData, encoding: .utf8)
+            session["_flash"] = stringData
         }
+        
         return self
     }
 }
