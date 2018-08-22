11be2091955a53320d0ef9becbec03fb79ee63c0
s2kinfra <daniel.skevarp@me.com>
2018-08-22 13:02:20 +0200

    Update FlashContainer.swift

--- Sources/Flash/Models/FlashContainer.swift
+++ Sources/Flash/Models/FlashContainer.swift
@@ -1,16 +1,11 @@
 import Vapor
 
 public final class FlashContainer: Codable, Service {
-    public var new: [Flash] = []
-    public var old: [Flash] = []
-
-    public var flashes: [Flash] {
-        get {
-            return new
-        }
-
-        set {
-            new = newValue
-        }
+    public var flashes: [Flash] = []
+    
+    public func clear() {
+        flashes = []
     }
+    
+    public init() { }
 }
