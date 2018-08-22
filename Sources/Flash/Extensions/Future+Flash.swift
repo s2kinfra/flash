2111a3186f0a6f54901a9fb3d7446561e02c7c1b
s2kinfra <daniel.skevarp@me.com>
2018-08-22 13:00:51 +0200

    Update Future+Flash.swift

--- Sources/Flash/Extensions/Future+Flash.swift
+++ Sources/Flash/Extensions/Future+Flash.swift
@@ -1,9 +1,9 @@
 import Vapor
 
 public extension Future where T: Response {
-    public func flash(_ type: Flash.Kind, _ message: String) -> Future<Response> {
+    public func flash(_ type: Flash.Kind, _ message: String, _ session: Session) -> Future<Response> {
         return self.map(to: Response.self) { res in
-            return res.flash(type, message)
+            return res.flash(type, message, session)
         }
     }
 }
